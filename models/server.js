6e37a389b6e7a9237e562462fcb62dea45f48beb
David_TC <davidcoraza0316@gmail.com>
2021-08-01 14:59:31 -0500

    Enable middleware to serve the directory public

--- models/server.js
+++ models/server.js
@@ -30,7 +30,7 @@ class Server{
         this.app.use( express.json() );
 
         // Servir el contenido del directorio 'public/  '
-        // this.app.use( express.static('public') );
+        this.app.use( express.static('public') );
     }
 
     routes(){
